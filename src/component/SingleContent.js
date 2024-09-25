eba2e843620daefc716b33c7d1418dded6674641
BbreeSwn <suwanan.cs@hotmail.com>
2024-09-25 14:43:15 +0700

    feat : set show state

--- src/component/SingleContent.js
+++ src/component/SingleContent.js
@@ -1,12 +1,13 @@
-import React from "react";
+import { useState } from "react";
 
 const SingleContent = ({title , description}) => {
+    const [show , setShow] = useState(false)
   return (<article className="content">
     <header>
         <h3>{title}</h3>
-        <button className="btn">+</button>
+        <button className="btn" onClick={()=>setShow(!show)}>{show? "-" : "+"}</button>
     </header>
-    <p>{description}</p>
+    {show && <p>{description}</p>}
 
   </article>);
 };
